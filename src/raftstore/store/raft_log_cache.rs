fa52ae5eeb4901125dba6fd4720b9d838cf47b46
Dylan.Wen <hhkbp2@gmail.com>
2016-12-20 17:52:46 +0800

    fix index bug

--- src/raftstore/store/raft_log_cache.rs
+++ src/raftstore/store/raft_log_cache.rs
@@ -84,8 +84,8 @@ impl RaftLogCacheCore {
 
     fn calc_idx(&self, index: u64) -> usize {
         let first_index = self.first_index().unwrap();
-        let offset = index - first_index;
-        (first_index + offset) as usize / self.buffer.len()
+        let offset = (index - first_index) as usize;
+        (self.start + offset) % self.buffer.len()
     }
 
     pub fn first_index(&self) -> raft::Result<u64> {
@@ -170,8 +170,9 @@ impl RaftLogCacheCore {
         if self.idle_size() == 0 {
             self.free()
         }
-        let idx = self.next_idx(self.end);
-        self.buffer[idx] = entry;
+
+        self.buffer[self.end] = entry;
+        self.end = self.next_idx(self.end);
     }
 
     fn append_entries(&mut self, entries: &[Entry]) {
@@ -225,10 +226,13 @@ impl RaftLogCacheCore {
         if self.stat.call % LOG_CALL_TIMES == 0 {
             info!("{} raft log cache stat: {:?}", self.tag, self.stat);
         }
+        if low == high {
+            return Ok(vec![]);
+        }
         let first_index = try!(self.first_index());
         let last_index = try!(self.last_index());
         self.stat.valid += 1;
-        if low < first_index || high > last_index {
+        if low < first_index || high > last_index + 1 {
             self.stat.miss += 1;
             return Err(raft::Error::Other(box_err!("miss cache")));
         }
@@ -236,15 +240,18 @@ impl RaftLogCacheCore {
         let mut current_size = 0;
         let total = high - low;
         let mut idx = self.calc_idx(low);
-        for _ in 0..total {
+        let entry = self.get_entry(idx);
+        current_size += Message::compute_size(&entry) as u64;
+        res.push(entry);
+        for _ in 1..total {
+            idx = self.next_idx(idx);
             let entry = self.get_entry(idx);
             current_size += Message::compute_size(&entry) as u64;
-            res.push(entry);
             if current_size > max_size {
                 self.stat.hit += 1;
                 return Ok(res);
             }
-            idx = self.next_idx(idx);
+            res.push(entry);
         }
         self.stat.hit += 1;
         Ok(res)
@@ -268,5 +275,107 @@ impl RaftLogCache {
     pub fn entries(&self, low: u64, high: u64, max_size: u64) -> raft::Result<Vec<Entry>> {
         let mut core = self.get();
         core.entries(low, high, max_size)
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use protobuf;
+    use kvproto::eraftpb::Entry;
+    use super::*;
+
+    fn new_entry(index: u64, term: u64) -> Entry {
+        let mut e = Entry::new();
+        e.set_index(index);
+        e.set_term(term);
+        e
+    }
+
+    fn size_of<T: protobuf::Message>(m: &T) -> u32 {
+        m.compute_size()
+    }
+
+    #[test]
+    fn test_raft_log_cache() {
+        let buffer_size = 5;
+        let tag = String::from("test");
+        // Initialize the test cases
+        let entries = vec![new_entry(3, 3), new_entry(4, 4), new_entry(5, 5), new_entry(6, 6)];
+        let max_u64 = u64::max_value();
+        let mut tests =
+            vec![// check bounds
+                 (2, 6, max_u64, true, vec![]),
+                 (3, 8, max_u64, true, vec![]),
+                 //
+                 (4, 5, max_u64, false, vec![new_entry(4, 4)]),
+                 (4, 6, max_u64, false, vec![new_entry(4, 4), new_entry(5, 5)]),
+                 (4, 7, max_u64, false, vec![new_entry(4, 4), new_entry(5, 5), new_entry(6, 6)]),
+                 // even if maxsize is zero, the first entry should be returned
+                 (4, 7, 0, false, vec![new_entry(4, 4)]),
+                 // limit size
+                 (4,
+                  7,
+                  (size_of(&entries[1]) + size_of(&entries[2])) as u64,
+                  false,
+                  vec![new_entry(4, 4), new_entry(5, 5)])];
+        // Initialize the cache
+        let cache = RaftLogCache::new(tag, buffer_size);
+        assert!(cache.get().is_empty());
+        if let Ok(i) = cache.get().first_index() {
+            panic!("unexpect first index {}", i);
+        }
+        if let Ok(i) = cache.get().last_index() {
+            panic!("unexpect first index {}", i);
+        }
+        cache.get().append(&entries);
+        assert!(!cache.get().is_empty());
+        assert_eq!(cache.get().len(), entries.len());
+        assert_eq!(cache.get().first_index(), Ok(3));
+        assert_eq!(cache.get().last_index(), Ok(6));
+
+        // Run the test cases
+        for (i, (low, high, maxsize, werror, wentries)) in tests.drain(..).enumerate() {
+            let r = cache.entries(low, high, maxsize);
+            if werror {
+                if let Ok(ents) = r {
+                    panic!("#{} expect error but got {:?}", i, ents);
+                }
+            } else {
+                if let Err(e) = r {
+                    panic!("#{} unexpect error {:?}", i, e);
+                }
+                let ents = r.unwrap();
+                if ents != wentries {
+                    panic!("#{}: expect entries {:?}, got {:?}", i, wentries, ents);
+                }
+            }
+        }
+
+        // Ensure compaction and truncation works
+        cache.get().compact_to(4);
+        assert_eq!(cache.get().len(), entries.len() - 1);
+        assert_eq!(cache.get().first_index(), Ok(4));
+        assert_eq!(cache.get().last_index(), Ok(6));
+
+        cache.get().truncate_to(6);
+        assert_eq!(cache.get().len(), entries.len() - 2);
+        assert_eq!(cache.get().first_index(), Ok(4));
+        assert_eq!(cache.get().last_index(), Ok(5));
+
+        // Append entries which outnumbers cache max_size
+        let more = (buffer_size + 1) as u64;
+        let last_index = cache.get().last_index().unwrap();
+        let mut entries = vec![];
+        for i in 0..more {
+            let index = last_index + 1 + i;
+            entries.push(new_entry(index, index));
+        }
+        cache.get().append(&entries);
+        let max_size = buffer_size - 1;
+        assert_eq!(cache.get().len(), max_size);
+        assert_eq!(cache.get().first_index(),
+                   Ok(last_index + more + 1 - max_size as u64));
+        assert_eq!(cache.get().last_index(), Ok(last_index + more));
     }
 }
